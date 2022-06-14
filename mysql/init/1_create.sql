f83e348dca14a6f70af932c408407be27cef73e7
grkon <yo.for.user@gmail.com>
2022-06-15 00:34:33 +0900

    init sql 追加

--- mysql/init/1_create.sql
+++ mysql/init/1_create.sql
@@ -2,12 +2,12 @@ DROP DATABASE IF EXISTS db_22spring;
 CREATE DATABASE db_22spring;
 USE db_22spring;
 
-CREATE TABLE `searchs` (
-    `id` CHAR(36) NOT NULL,
+CREATE TABLE `searches` (
+    `id` CHAR(36) PRIMARY KEY NOT NULL,
     `station` VARCHAR(100) DEFAULT NULL,
     `lat` DOUBLE(9, 6) DEFAULT NULL,
     `lng` DOUBLE(9, 6) DEFAULT NULL,
-    `created_at` DATETIME(6) DEFAULT NULL
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `questions` (
@@ -15,33 +15,39 @@ CREATE TABLE `questions` (
     `shop_id` CHAR(10) NOT NULL,
     `search_id` CHAR(36) NOT NULL,
     `number` INT NOT NULL,
-    `created_at` DATETIME(6) DEFAULT NULL
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`search_id`) REFERENCES `searches`(`id`),
+    FOREIGN KEY (`shop_id`) REFERENCES `shops`(`shop_id`)
 );
 
 CREATE TABLE `shops` (
-    `shop_id` CHAR(10) NOT NULL,
+    `shop_id` CHAR(10) PRIMARY KEY NOT NULL,
     `name` VARCHAR(200) NOT NULL,
     `image` VARCHAR(2048) NOT NULL,
     `genre_code` CHAR(4) NOT NULL,
     `subgenre_code` CHAR(4) DEFAULT NULL,
     `price_code` CHAR(4) NOT NULL,
-    `created_at` DATETIME(6) DEFAULT NULL
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (`genre_code`) REFERENCES `genres`(`genre_code`),
+    FOREIGN KEY (`subgenre_code`) REFERENCES `genres`(`genre_code`),
+    FOREIGN KEY (`price_code`) REFERENCES `prices`(`price_code`)
 );
 
 CREATE TABLE `genres` (
-    `genre_code` CHAR(4) NOT NULL,
+    `genre_code` CHAR(4) PRIMARY KEY NOT NULL,
     `name` VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE `prices` (
-    `price_code` CHAR(4) NOT NULL,
+    `price_code` CHAR(4) PRIMARY KEY NOT NULL,
     `name` VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE `gourmets` (
+    `id` INT PRIMARY KEY NOT NULL,
     `station` VARCHAR(100) DEFAULT NULL,
     `lat` DOUBLE(9, 6) DEFAULT NULL,
     `lng` DOUBLE(9, 6) DEFAULT NULL,
     `shops` TEXT NOT NULL,
-    `created_at` DATETIME(6) DEFAULT NULL
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP
 );
