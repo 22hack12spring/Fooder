bae0080dab4547d667d35a7fc14b33f6f2d19daa
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-06-15 01:00:08 +0900

    bugfix

--- mysql/init/1_create.sql
+++ mysql/init/1_create.sql
@@ -3,51 +3,51 @@ CREATE DATABASE db_22spring;
 USE db_22spring;
 
 CREATE TABLE `searches` (
-    `id` CHAR(36) PRIMARY KEY NOT NULL,
+    `id` CHAR(36) PRIMARY KEY,
     `station` VARCHAR(100) DEFAULT NULL,
     `lat` DOUBLE(9, 6) DEFAULT NULL,
     `lng` DOUBLE(9, 6) DEFAULT NULL,
-    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `questions` (
-    `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `shop_id` CHAR(10) NOT NULL,
-    `search_id` CHAR(36) NOT NULL,
-    `number` INT NOT NULL,
-    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (`search_id`) REFERENCES `searches`(`id`),
-    FOREIGN KEY (`shop_id`) REFERENCES `shops`(`shop_id`)
+CREATE TABLE `genres` (
+    `genre_code` CHAR(4) PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE `prices` (
+    `price_code` CHAR(4) PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE `shops` (
-    `shop_id` CHAR(10) PRIMARY KEY NOT NULL,
+    `shop_id` CHAR(10) PRIMARY KEY,
     `name` VARCHAR(200) NOT NULL,
     `image` VARCHAR(2048) NOT NULL,
     `genre_code` CHAR(4) NOT NULL,
     `subgenre_code` CHAR(4) DEFAULT NULL,
     `price_code` CHAR(4) NOT NULL,
-    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (`genre_code`) REFERENCES `genres`(`genre_code`),
     FOREIGN KEY (`subgenre_code`) REFERENCES `genres`(`genre_code`),
     FOREIGN KEY (`price_code`) REFERENCES `prices`(`price_code`)
 );
 
-CREATE TABLE `genres` (
-    `genre_code` CHAR(4) PRIMARY KEY NOT NULL,
-    `name` VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE `prices` (
-    `price_code` CHAR(4) PRIMARY KEY NOT NULL,
-    `name` VARCHAR(100) NOT NULL
+CREATE TABLE `questions` (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `shop_id` CHAR(10) NOT NULL,
+    `search_id` CHAR(36) NOT NULL,
+    `number` INT NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`search_id`) REFERENCES `searches`(`id`),
+    FOREIGN KEY (`shop_id`) REFERENCES `shops`(`shop_id`)
 );
 
 CREATE TABLE `gourmets` (
-    `id` INT PRIMARY KEY NOT NULL,
+    `id` INT PRIMARY KEY,
     `station` VARCHAR(100) DEFAULT NULL,
     `lat` DOUBLE(9, 6) DEFAULT NULL,
     `lng` DOUBLE(9, 6) DEFAULT NULL,
     `shops` TEXT NOT NULL,
-    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
