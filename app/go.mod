b8a7976c50b2e978e9021bb6a844a4d8625ec7df
grkon <yo.for.user@gmail.com>
2022-06-16 01:26:41 +0900

    CreateSearchを作成

--- app/go.mod
+++ app/go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/uuid v1.3.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/labstack/echo/v4 v4.7.2
 )
--- app/model/searches.go
+++ app/model/searches.go
@@ -1,6 +1,10 @@
 package model
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
 
 type SearchesRepository interface {
 	CreateSearch(arg SearchArgs) (error, Searches)
@@ -22,6 +26,41 @@ type Searches struct {
 
 // CreateSearch　uuidを発行して、Searchesテーブルにデータを追加する
 func (repo *SqlxRepository) CreateSearch(arg SearchArgs) (error, Searches) {
-	// implement here
-	return nil, Searches{}
+	u, err := uuid.NewRandom()
+
+	if err != nil {
+		return err, Searches{}
+	}
+
+	var station sql.NullString
+	var lat sql.NullFloat64
+	var lng sql.NullFloat64
+
+	if arg.Station == nil {
+		station.String, station.Valid = "", false
+	} else {
+		station.String, station.Valid = *arg.Station, true
+	}
+	if arg.Lat == nil {
+		lat.Float64, lat.Valid = 0, false
+	} else {
+		lat.Float64, lat.Valid = *arg.Lat, true
+	}
+	if arg.Lng == nil {
+		lng.Float64, lng.Valid = 0, false
+	} else {
+		lng.Float64, lng.Valid = *arg.Lat, true
+	}
+
+	search := &Searches{
+		ID: u.String(),
+		Station: station,
+		Lat: lat,
+		Lng: lng,
+	}
+
+	sql := "INSERT INTO searches (id, station, lat, lng) VALUE (?, ?, ?, ?)"
+	repo.db.Exec(sql, search.ID, search.Station, search.Lat, search.Lng)
+
+	return nil, *search
 }
