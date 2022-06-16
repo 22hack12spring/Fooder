24c69e0fad4b8077feca55be914fffbc27a72835
grkon <yo.for.user@gmail.com>
2022-06-17 00:05:43 +0900

    CreateQuestionsを作成

--- app/model/questions.go
+++ app/model/questions.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"github.com/google/uuid"
+)
+
 type QuestionsRepository interface {
 	CreateQuestions(args QuestionArgs) ([7]Questions, error)
 }
@@ -18,6 +22,25 @@ type Questions struct {
 }
 
 // CreateQuestions  質問データと search_id から Questions テーブルにデータを追加する
-func (repo *SqlxRepository) CreateQuestions(args QuestionArgs) ([7]Questions, error) {
-	return [7]Questions{}, nil
+func (repo *SqlxRepository) CreateQuestions(args QuestionArgs) (questions [7]Questions, err error) {
+	var u uuid.UUID
+	
+	sql := "INSERT questions (id, shop_id, search_id, number) VALUES (?, ?, ?, ?)"
+
+	for i, s := range(args.Shop_ids) {
+		u = uuid.New()
+		questions[i].ID = u.String()
+		questions[i].Shop_id = s
+		questions[i].Search_id = *args.Search_id
+		questions[i].Number = i
+
+		_, err = repo.db.DB.Exec(sql, &questions[i].ID, &questions[i].Shop_id, &questions[i].Search_id, &questions[i].Number)
+
+		if err != nil {
+			questions = [7]Questions{}
+			return
+		}
+	}
+	
+	return
 }
