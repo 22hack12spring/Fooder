a243a6abf87d818dba60d1a0c2ad9c333fbc1ecc
Luftalian <springtechtree@gmail.com>
2022-06-16 21:10:11 +0900

    pricesです。

--- app/model/prices.go
+++ app/model/prices.go
@@ -1,9 +1,25 @@
 package model
 
+import "context"
+
 type Price struct {
-	GenreCode string `db:"genre_code"`
+	PriceCode string `db:"price_code"`
 	Name      string `db:"name"`
 }
 
 type PricesRepository interface {
+	GetPrices() ([]*Price, error)
+	PriceCodeToName(ctx context.Context, code string) (string, error)
+}
+
+func (repo *SqlxRepository) GetPrices() ([]*Price, error) {
+	return nil, nil
+}
+
+func (repo *SqlxRepository) PriceCodeToName(ctx context.Context, code string) (string, error) {
+	var name string
+	if err := repo.db.Get(&name, "SELECT name FROM prices WHERE price_code=?", code); err != nil {
+		return "", err
+	}
+	return name, nil
 }
