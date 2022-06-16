e5e6bcda8a4669ac5d6f2db557ac48a0d6bffc10
Luftalian <springtechtree@gmail.com>
2022-06-16 20:20:00 +0900

    フォーマットしました。

--- app/router/ping.go
+++ app/router/ping.go
@@ -10,11 +10,5 @@ import (
 // /api/ping
 func (h *Handlers) Ping(c echo.Context) error {
 	log.Println("ping recieved")
-	name, err := h.Repo.GenreCodeToName(c.Request().Context(), "G001")
-	if err != nil {
-		return err
-	}
-	log.Println(name)
-	return c.String(http.StatusOK, name)
-
+	return c.String(http.StatusOK, "pong")
 }
