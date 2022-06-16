57e92ba93b66c81003045eded0bbb0e71ab5fd4c
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-06-16 21:49:29 +0900

    sampleを追加

---
+++ mysql/init/3_shops.sql
@@ -0,0 +1,25 @@
+SET character_set_client = utf8mb4;
+SET CHARACTER_SET_CONNECTION = utf8mb4;
+
+INSERT INTO `shops` (`shop_id`, `name`, `image`, `genre_code`, `subgenre_code`, `price_code`) 
+VALUES
+ ('J001218670', '原価ビストロBAN! 横浜西口天理ビル', 'https://imgfp.hotp.jp/IMGH/64/68/P033266468/P033266468_168.jpg', 'G001', 'G006', 'B003'),
+ ('J000302336', '松尾ジンギスカン 本店', 'https://imgfp.hotp.jp/IMGH/45/45/P038004545/P038004545_168.jpg', 'G008', 'G004', 'B004'),
+ ('J001281884', 'OAK BAR オークバー 渋谷店', 'https://imgfp.hotp.jp/IMGH/55/01/P039055501/P039055501_168.jpg', 'G002', 'G001', 'B003'),
+ ('J001284246', 'イタリアンバル NORANEKO', 'https://imgfp.hotp.jp/IMGH/57/26/P038965726/P038965726_168.jpg', 'G006', 'G002', 'B003'),
+ ('J000348080', '幸寿司 福井', 'https://imgfp.hotp.jp/IMGH/75/07/P013967507/P013967507_168.jpg', 'G004', NULL, 'B006'),
+ ('J001118127', '横濱こてがえし', 'https://imgfp.hotp.jp/IMGH/44/04/P021654404/P021654404_168.jpg', 'G016', NULL, 'B001'),
+ ('J000000711', '渋谷食堂 Ventuno Tokyo ヴェントゥーノ 東京', 'https://imgfp.hotp.jp/IMGH/21/50/P038942150/P038942150_168.jpg', 'G005', NULL, 'B003'),
+ ('J001277008', 'シェーキーズ 渋谷店', 'https://imgfp.hotp.jp/IMGH/83/48/P036378348/P036378348_168.jpg', 'G006', NULL, 'B001'),
+ ('J001259105', 'FLIPPERS 渋谷店', 'https://imgfp.hotp.jp/IMGH/46/58/P037414658/P037414658_168.jpg', 'G014', NULL, 'B001'),
+ ('J000805249', '立吉餃子 渋谷本店', 'https://imgfp.hotp.jp/IMGH/47/83/P029444783/P029444783_168.jpg', 'G007', NULL, 'B002'),
+ ('J000010417', 'ズーガンズー ZOOGUNZOO 渋谷店', 'https://imgfp.hotp.jp/IMGH/72/39/P023077239/P023077239_168.jpg', 'G002', NULL, 'B004'),
+ ('J001259333', 'らーめん直久 青山店', 'https://imgfp.hotp.jp/IMGH/00/37/P036780037/P036780037_168.jpg', 'G013', NULL, 'B001'),
+ ('J001134380', '味道本色', 'https://imgfp.hotp.jp/IMGH/37/04/P023243704/P023243704_168.jpg', 'G007', NULL, 'B002'),
+ ('J001258716', 'AWkitchen figlia 渋谷スクランブルスクエア', 'https://imgfp.hotp.jp/IMGH/31/46/P036683146/P036683146_168.jpg', 'G006', NULL, 'B002'),
+ ('J001280452', 'グランソル東京 渋谷ミヤシタパーク店', 'https://imgfp.hotp.jp/IMGH/20/13/P038312013/P038312013_168.jpg', 'G002', NULL, 'B002'),
+ ('J001291668', 'マモケバブ 吉祥寺店', 'https://imgfp.hotp.jp/IMGH/40/39/P039064039/P039064039_168.jpg', 'G010', NULL, 'B010'),
+ ('J000659185', '溶岩焼肉ダイニング bonbori 渋谷道玄坂店', 'https://imgfp.hotp.jp/IMGH/62/35/P033996235/P033996235_168.jpg', 'G008', NULL, 'B008'),
+ ('J001259497', 'Dining Bar Aquavit アクアビット', 'https://imgfp.hotp.jp/IMGH/77/92/P036767792/P036767792_168.jpg', 'G012', NULL, 'B004'),
+ ('J001127605', '渋谷鳥田むら', 'https://imgfp.hotp.jp/IMGH/97/50/P022859750/P022859750_168.jpg', 'G001', NULL, 'B003'),
+ ('J000963525', '寿司の磯松', 'https://imgfp.hotp.jp/IMGH/01/29/P038360129/P038360129_168.jpg', 'G001', NULL, 'B004');
