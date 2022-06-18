1f9bbbc470d61095ae056ba19ed9143d4b79106d
grkon <yo.for.user@gmail.com>
2022-06-18 13:20:39 +0900

    GetPricesを実装

--- app/model/prices.go
+++ app/model/prices.go
@@ -1,19 +1,50 @@
 package model
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 type Price struct {
 	PriceCode string `db:"price_code"`
 	Name      string `db:"name"`
 }
 
+type PriceCache struct {
+	Name map[string]string
+	Mux  sync.RWMutex
+}
+
+var PriceCacheData PriceCache
+
 type PricesRepository interface {
-	GetPrices() ([]*Price, error)
+	GetPrices() ([]Price, error)
 	PriceCodeToName(ctx context.Context, code string) (string, error)
 }
 
-func (repo *SqlxRepository) GetPrices() ([]*Price, error) {
-	return nil, nil
+func (repo *SqlxRepository) GetPrices() ([]Price, error) {
+	sql := "Select * FROM prices"
+
+	var prices []Price
+
+	err := repo.db.Select(&prices, sql)
+
+	if err != nil {
+		return []Price{}, err
+	}
+
+	PriceCacheData.Mux.Lock()
+
+	if PriceCacheData.Name == nil {
+		PriceCacheData.Name = make(map[string]string)
+	}
+
+	for _, p := range(prices) {
+		PriceCacheData.Name[p.PriceCode] = p.Name
+	}
+	PriceCacheData.Mux.Unlock()
+
+	return prices, nil
 }
 
 func (repo *SqlxRepository) PriceCodeToName(ctx context.Context, code string) (string, error) {
